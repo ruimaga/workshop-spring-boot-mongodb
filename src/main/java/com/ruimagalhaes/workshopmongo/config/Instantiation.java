a68687d2dae1139680ac359102ee70a3573d18ad
Rui Magalhães <ruimaga.meirelles@gmail.com>
2022-02-18 07:01:34 -0300

    Operação de instanciação da base de dados mongoDB

---
+++ src/main/java/com/ruimagalhaes/workshopmongo/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.ruimagalhaes.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.ruimagalhaes.workshopmongo.domain.User;
+import com.ruimagalhaes.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll(); //deleta toda base de dados
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User rui = new User(null, "Rui Magalhaes", "rui@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob,rui));
+		
+	}
+
+}
